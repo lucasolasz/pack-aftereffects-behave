ef1bf2ef16d4b6a2fc7f47e4c15452a7d2af5027
lucasolasz <olaszlucas072@gmail.com>
2025-07-07 00:53:08 -0300

    Hero inicial

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,7 +1,28 @@
+import { ButtonComprar } from "./button-comprar";
+
 export function Hero() {
   return (
     <div>
-      <h1>Hero</h1>
+      <div className="w-full flex items-center justify-center">
+        <div className="hero-background max-w-[1240px] mx-auto grid md:grid-cols-2 h-150 rounded-4xl">
+          <div className="flex flex-col justify-center">
+            <h1
+              className="gradient-text
+            text-4xl md:text-5xl font-bold text-white mb-4 "
+            >
+              Crie Vídeos Profissionais em Minutos com Nosso Pack Exclusivo de
+              After Effects
+            </h1>
+
+            <p className="text-lg md:text-xl mb-6 text-gray-300 font-bold">
+              Transforme suas ideias em animações incríveis com templates 100%
+              editáveis, prontos para uso e perfeitos para qualquer projeto.
+            </p>
+
+            <ButtonComprar>Comprar Agora</ButtonComprar>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
