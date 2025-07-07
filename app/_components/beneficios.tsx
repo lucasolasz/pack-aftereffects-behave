7564ba6d335459734a4ce3f6ae41ab2355c8996d
lucasolasz <olaszlucas072@gmail.com>
2025-07-07 01:04:36 -0300

    Inclui na seção

---
+++ app/_components/beneficios.tsx
@@ -0,0 +1,16 @@
+export default function Beneficios() {
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="w-full flex items-center justify-center flex-col">
+        <h1 className="text-4xl font-bold text-center cor-azul-site">
+          Por que nosso pack de After Effects é perfeito para você?
+        </h1>
+        <p className="text-lg md:text-xl mb-6 text-gray-600 font-bold text-center mt-4">
+          Descubra como nossos templates vão elevar seus projetos a outro nível.
+        </p>
+      </div>
+
+      <div className="max-w-[1400px] mx-auto grid md:grid-cols-2"></div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import { Hero } from "./_components/hero";
 import { NavBar } from "./_components/navbar";
+import Beneficios from "./_components/beneficios";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Beneficios />
     </div>
   );
 }
