372824c22945faa9a808924f0b3085d561161dd0
lucasolasz <olaszlucas072@gmail.com>
2025-07-07 12:48:15 -0300

    Correção do metadata

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export const metadata: Metadata = {
     "Animações para Redes Sociais",
   ],
   authors: [{ name: "[Vitor Letta - Behave]" }],
-  viewport: "width=device-width, initial-scale=1.0",
   robots: {
     index: true,
     follow: true,
@@ -28,11 +27,11 @@ export const metadata: Metadata = {
     title: "Pack de Templates After Effects - Crie Vídeos Incríveis",
     description:
       "Transforme seus projetos com nosso pack de templates editáveis para After Effects. Crie vídeos profissionais rapidamente e impressione seus clientes!",
-    url: "[Insira a URL da sua landing page, ex: https://br.pinterest.com/vitorleta/?invite_code=a9e05720bd2d4da48977f288b14430a2]",
-    siteName: "[Nome do Site ou Marca, ex: MeuPackAfterEffects]",
+    url: "https://pack-aftereffects-behave.vercel.app",
+    siteName: "Behave Templates",
     images: [
       {
-        url: "[https://i.pinimg.com/280x280_RS/32/1e/ba/321eba51584233268728895cad55eba2.jpg]",
+        url: "https://i.pinimg.com/280x280_RS/32/1e/ba/321eba51584233268728895cad55eba2.jpg",
         width: 1200,
         height: 630,
         alt: "Preview do Pack de Templates After Effects",
@@ -47,7 +46,7 @@ export const metadata: Metadata = {
     description:
       "Crie vídeos profissionais em minutos com nosso pack de templates editáveis para After Effects. Perfeito para criadores e agências!",
     images: [
-      "[https://i.pinimg.com/280x280_RS/32/1e/ba/321eba51584233268728895cad55eba2.jpg]",
+      "https://i.pinimg.com/280x280_RS/32/1e/ba/321eba51584233268728895cad55eba2.jpg",
     ],
   },
 };
