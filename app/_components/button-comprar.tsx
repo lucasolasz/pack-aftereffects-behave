1b75386247f3a4c2424995a87d813960ecee43b3
lucasolasz <olaszlucas072@gmail.com>
2025-07-07 10:03:53 -0300

    Corrige tamanho fixo do botão

--- app/_components/button-comprar.tsx
+++ app/_components/button-comprar.tsx
@@ -6,7 +6,7 @@ type ButtonComprarProps = {
 
 export function ButtonComprar({ children }: ButtonComprarProps) {
   return (
-    <button className="w-100 cursor-pointer bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-white text-lg font-semibold px-4 py-2 rounded-full border-none shadow-none transition duration-300 hover:from-pink-600 hover:via-purple-600 hover:to-blue-600">
+    <button className="px-20 py-2 cursor-pointer bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-white text-lg font-semibold rounded-full border-none shadow-none transition duration-300 hover:from-pink-600 hover:via-purple-600 hover:to-blue-600">
       {children}
     </button>
   );
