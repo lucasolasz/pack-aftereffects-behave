9baa8d7fc83f6e899812c42b6ce521ff1bd09efe
lucasolasz <olaszlucas072@gmail.com>
2025-07-07 00:43:22 -0300

    Inclusão dos dois componentes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Hero } from "./_components/hero";
+import { NavBar } from "./_components/navbar";
 
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <Hero />
     </div>
   );
