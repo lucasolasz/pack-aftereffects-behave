6ffef7d4a0bb1bcd726359f89be885a6fee9686b
lucasolasz <olaszlucas072@gmail.com>
2025-08-01 20:48:26 -0300

    More packs

--- app/_components/morePacks.tsx
+++ app/_components/morePacks.tsx
@@ -3,13 +3,24 @@ import Pack from "./pack";
 export default function MorePacks() {
   return (
     <div className="w-full flex flex-col items-center bg-cor-azul-site px-4">
-      <h1 className="text-white font-extrabold text-4xl my-4 md:text-6xl">
+      <h1 className="text-white font-extrabold text-4xl my-6 md:text-6xl">
         MORE PACKS
       </h1>
 
-      <div className="max-w-[1000px] mx-auto flex flex-col items-center justify-center ">
+      <div className="max-w-[1000px] mx-auto flex flex-col items-center justify-center my-5">
         <div className="flex gap-4 m-3 md:grid md:grid-cols-2 md:m-0">
-          <Pack />
+          <Pack
+            tituloPack="Pack 01"
+            subTituloPack1="Big Pack"
+            subTituloPack2="Instagram"
+            descricao1="640 Animations and Effects"
+          />
+          <Pack
+            tituloPack="Pack 02"
+            subTituloPack1="Gaze"
+            subTituloPack2="Graphic Pack"
+            descricao1="4500 Animations and Effects"
+          />
         </div>
       </div>
     </div>
--- app/_components/pack.tsx
+++ app/_components/pack.tsx
@@ -2,14 +2,22 @@ import Image from "next/image";
 import { ButtonComprarPacks } from "./button-comprar-packs";
 
 type PackProps = {
-  titulo: string;
+  tituloPack: string;
+  subTituloPack1: string;
+  subTituloPack2: string;
   descricao1: string;
-  descricao2: string;
 };
 
-export default function Pack() {
+export default function Pack({
+  tituloPack,
+  subTituloPack1,
+  subTituloPack2,
+  descricao1,
+}: PackProps) {
   return (
     <div className="flex flex-col">
+      <h1 className="text-white font-extrabold text-4xl mb-3">{tituloPack}</h1>
+
       <Image
         src="/images/pack01.jpg"
         alt="Pack 01 image"
@@ -17,11 +25,11 @@ export default function Pack() {
         height={320}
         className="rounded-lg mb-3"
       />
-      <h1 className="text-white font-extrabold">Big Pack</h1>
-      <h1 className="text-white font-extrabold">Instagram</h1>
-      <p className="text-white text-sm my-2">640 Animações e Efeitos</p>
+      <h1 className="text-white font-extrabold">{subTituloPack1}</h1>
+      <h1 className="text-white font-extrabold">{subTituloPack2}</h1>
+      <p className="text-white text-sm my-2">{descricao1}</p>
       <button className="bg-gray-400 w-full rounded mb-3 font-semibold cursor-pointer">
-        Ver Animações
+        See animations
       </button>
       <ButtonComprarPacks>Buy Now</ButtonComprarPacks>
     </div>
