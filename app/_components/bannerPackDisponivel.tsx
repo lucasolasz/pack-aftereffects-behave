1d3157019de5f4969183a6856d8f8d892f785437
conuaz <lucas.olasz@tty2000.com.br>
2025-08-01 18:05:53 -0300

    Aceita 2 descrições

--- app/_components/bannerPackDisponivel.tsx
+++ app/_components/bannerPackDisponivel.tsx
@@ -4,11 +4,13 @@ import { MdOutlineFileDownload } from "react-icons/md";
 type BannerPackDisponivelProps = {
   titulo: string;
   descricao: string;
+  descricao2?: string;
 };
 
 export function BannerPackDisponivel({
   titulo,
   descricao,
+  descricao2,
 }: BannerPackDisponivelProps) {
   return (
     <div className="max-w-4xl md:w-[550px] min-h-[80px] flex justify-between bg-degrade-promopack rounded p-5">
@@ -16,8 +18,11 @@ export function BannerPackDisponivel({
         <h1 className="font-extrabold text-2xl md:text-4xl text-white font-montserrat">
           {titulo}
         </h1>
-        <p className="text-white leading-relaxed font-[150] md:text-base">
+        <p className="text-white leading-relaxed font-[150] md:text-base m-0">
           {descricao}
+        </p>
+        <p className="text-white leading-relaxed font-[150] md:text-base m-0">
+          {descricao2}
         </p>
       </div>
       <div className="flex items-center">
