43b49d28877022d74c67376e1a8523f936ba08b6
lucasolasz <olaszlucas072@gmail.com>
2025-08-01 01:02:54 -0300

    Primeira versao do packpromo1

--- app/_components/beneficios.tsx
+++ app/_components/beneficios.tsx
@@ -1,7 +1,5 @@
-import { CardBeneficioDireita } from "./cardBeneficioDireita";
-import { CardBeneficioEsquerda } from "./cardBeneficioEsquerda";
-
 import { BannerPackDisponivel } from "./bannerPackDisponivel";
+import { ButtonComprar } from "./button-comprar";
 import VideoPack from "./videoPack";
 
 export default function Beneficios() {
@@ -13,32 +11,66 @@ export default function Beneficios() {
       />
 
       <div className="max-w-[1000px] mx-auto flex flex-col items-center justify-center bg-cor-azul-site">
-        <div className="flex items-center mx-4 mt-4">
+        <div className="md:grid md:grid-cols-2 flex gap-4 p-3">
           <VideoPack src="/videos/promo_pack_01/coffe.mp4" />
 
           <VideoPack src="/videos/promo_pack_01/tenis_nike.mp4" />
         </div>
-        <div className="flex items-center mx-4">
+
+        <div className="md:grid md:grid-cols-2 flex gap-4 p-3 md:p-0">
           <VideoPack src="/videos/promo_pack_01/spider.mp4" />
           <VideoPack src="/videos/promo_pack_01/ghost.mp4" />
         </div>
 
-        <div className="flex items-center mx-4">
+        <div className="grid grid-cols-2 gap-4 p-3 items-center">
           <VideoPack src="/videos/promo_pack_01/michael_jordan.mp4" />
-          <div className="h- bg-degrade-venda">
-            <h1>Project</h1>
-            <h1>Features</h1>
-            <p>
-              <span>5</span> individual ".aep" files
-            </p>
-            <p>Completely editable</p>
-            <p>No plug-ins needed</p>
-            <p>Assets Included</p>
-            <p>Use it in your work</p>
-            <p>Compatible with Windows and MacOS</p>
-            <p>Amazing Time Saver</p>
+          <div className="h-full py-4">
+            <div className="h-full p-3 bg-degrade-venda">
+              <h1 className="font-extrabold text-2xl text-white font-montserrat  md:text-6xl">
+                Project
+              </h1>
+              <h1 className="font-extrabold text-2xl text-white font-montserrat md:text-6xl">
+                Features
+              </h1>
+              <p className="text-white text-sm mt-2 md:text-base md:my-5">
+                - <span className="font-bold text-white">5</span> individual
+                ".aep" files
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - Completely editable
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - No plug-ins needed
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - Assets Included
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - Use it in your work
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - Compatible with Windows and MacOS
+              </p>
+              <p className="text-white text-sm my-1 md:text-base md:my-5">
+                - Amazing Time Saver
+              </p>
+              <div className="flex items-center justify-center mt-5">
+                <span className="text-[10px] text-white md:text-[40px]">$</span>
+                <h1 className="text-3xl text-white font-bold md:text-7xl ">
+                  5
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
+        <ButtonComprar>Buy now</ButtonComprar>
+        <p className="text-white mt-5">100% secure purchase.</p>
+        <p className="text-white">One-time payment and lifetime access</p>
+        <img
+          src="/images/cartoes.svg"
+          alt="Cartões aceitos"
+          className="mt-6 w-100 h-auto mx-auto"
+        />
       </div>
     </div>
   );
