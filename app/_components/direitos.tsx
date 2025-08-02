473c664710eea39291420de0dc83191456015c3a
lucasolasz <olaszlucas072@gmail.com>
2025-08-01 22:44:04 -0300

    Corrige centralização

--- app/_components/direitos.tsx
+++ app/_components/direitos.tsx
@@ -2,7 +2,7 @@ export function Direitos() {
   const ano = new Date().getFullYear();
   return (
     <div className="w-full bg-white">
-      <div className="flex flex-col items-center text-end max-w-500 mx-auto p-5">
+      <div className="flex flex-col items-center text-center max-w-500 mx-auto p-5">
         <p className="text-gray-500">
           MOTION BLENDER© {ano} Todos os Direitos Reservados
         </p>
